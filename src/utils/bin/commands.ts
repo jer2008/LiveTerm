0a4585ae764a4b845fe5c408bb154a8422c577d6
jer2008 <117557416+jer2008@users.noreply.github.com>
2022-12-19 20:48:27 +0800

    Update commands.ts

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -139,7 +139,7 @@ export const banner = (args?: string[]): string => {
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
 Type 'readme' to display github readme.
-Type 'quate' to display sayings.
+Type 'quote' to display sayings.
 Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
 `;
 };
